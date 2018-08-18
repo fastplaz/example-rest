63495cfa7e08d8de5854415254b46ac0b005e043
Luri <luri@kioss.com>
2018-08-18 21:54:20 +0700

    automation test

---
+++ test-automation/test/Config.js
@@ -0,0 +1,14 @@
+let Config = {
+  environment: 'development',
+  production: {
+    baseURL: 'https://your-url'
+  },
+  beta: {
+    baseURL: 'https://your-url'
+  },
+  development: {
+    baseURL: 'localhost/api-test/customer'
+  }
+}
+
+module.exports = Config;
